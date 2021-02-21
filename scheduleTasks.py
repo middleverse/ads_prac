e20f44234f59fd79871e80886de35826519f9201
middleverse <arshi_b@hotmail.com>
2021-02-21 07:02:29 -0500

    binary search

---
+++ scheduleTasks.py
@@ -0,0 +1,15 @@
+def schedule_tasks(tasks, k):
+    intervalCount = 0
+    # TODO: Write your code here
+    return intervalCount
+
+
+def main():
+  print("Minimum intervals needed to execute all tasks: " +
+        str(schedule_tasks(['a', 'a', 'a', 'b', 'c', 'c'], 2)))
+  print("Minimum intervals needed to execute all tasks: " +
+        str(schedule_tasks(['a', 'b', 'a'], 3)))
+
+
+main()
+
